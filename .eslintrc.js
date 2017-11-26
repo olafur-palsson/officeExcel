3b8d87889b7f96698354038dbca4823254e23d31
Óli P :) <olafur.palsson2@gmail.com>
2017-11-26 19:36:24 +0000

    First commit, finished getting availability data from Roomer, data representation is now available also. Made rate calculation parameters and those are loaded into the cache too. Next step will be to calculate the rates for each date via a formula.

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: 'airbnb-base',
+  env: {
+    browser: true,
+  },
+  rules: {
+    'class-methods-use-this': 0,
+    'semi': 0,
+    'no-multi-spaces': 0,
+    'no-whitespace-before-property': 0,
+    'brace-style': 0,
+    'no-param-reassign': 0,
+    'max-len': 0,
+  }
+};
